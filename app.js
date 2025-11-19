914cbdff0a53097ece7bfd49f590c3a802c62706
Your Name <you@example.com>
2025-11-19 14:59:27 +0200

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // your code goes here
+const buttonCalc = document.getElementById("calc-button")
+const inputField1 = document.getElementById("input1")
+const inputField2 = document.getElementById("input2")
+const outputVal = document.getElementById("output")
+
+
+    buttonCalc.addEventListener('click', calcBMI)
+
+    function calcBMI(){
+        let sum = 0;
+        sum = sum + inputField1.value / inputField2.value
+
+    sum.innerHTML = outputVal
+    }
