abbb79f682bde6306c8bd14f7d23377947194a49
Your Name <you@example.com>
2025-11-19 15:31:04 +0200

    files modified

--- app.js
+++ app.js
@@ -2,14 +2,17 @@
 const buttonCalc = document.getElementById("calc-button")
 const inputField1 = document.getElementById("input1")
 const inputField2 = document.getElementById("input2")
-const outputVal = document.getElementById("output")
-
+const outputVal = document.getElementById("value-output")
+const outputBMI = document.getElementById("bmi-output")
 
     buttonCalc.addEventListener('click', calcBMI)
 
     function calcBMI(){
         let sum = 0;
-        sum = sum + inputField1.value / inputField2.value
+        output =  inputField1.value / inputField2.value
 
-    sum.innerHTML = outputVal
+        outputVal.innerHTML = output
+
     }
+
+
